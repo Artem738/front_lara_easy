d87b5ab34a8d7ae836364a35c55114ff2b70e5e6
ArtRyzen <artembabichev@gmail.com>
2023-07-26 06:36:32 +0300

    provider init

--- lib/screens/screen_two.dart
+++ lib/screens/screen_two.dart
@@ -2,9 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../provider/pr.dart';
 
-class ScreenTwo extends StatelessWidget {
+class ScreenTwo extends StatefulWidget {
+  @override
+  _ScreenTwoState createState() => _ScreenTwoState();
+}
+
+class _ScreenTwoState extends State<ScreenTwo> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    final pr = Provider.of<Pr>(context, listen: false);
+    pr.fetchData(
+      startDate: DateTime.now(),
+      endDate: DateTime.now(),
+      selectedLanguage: 'en',
+      selectedYear: '2022',
+    );
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    final double maxScroll = _scrollController.position.maxScrollExtent;
+    final double currentScroll = _scrollController.position.pixels;
+    final double delta = 200.0; // Минимальное расстояние для начала загрузки новых данных
+
+    if (maxScroll - currentScroll <= delta) {
+      final pr = Provider.of<Pr>(context, listen: false);
+      pr.fetchData(
+        startDate: DateTime.now(),
+        endDate: DateTime.now(),
+        selectedLanguage: 'en',
+        selectedYear: '2022',
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final pr = Provider.of<Pr>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Books List'),
@@ -20,7 +65,6 @@ class ScreenTwo extends StatelessWidget {
                 // ... ваш код для верхнего меню ...
                 ElevatedButton(
                   onPressed: () {
-                    final pr = Provider.of<Pr>(context, listen: false);
                     pr.fetchData(
                       startDate: DateTime.now(),
                       endDate: DateTime.now(),
@@ -35,26 +79,25 @@ class ScreenTwo extends StatelessWidget {
           ),
           // Список книг
           Expanded(
-            child: Consumer<Pr>(
-              builder: (context, pr, _) {
-                if (pr.isLoading) {
-                  return Center(
-                    child: CircularProgressIndicator(),
+            child: RefreshIndicator(
+              onRefresh: () => pr.fetchData(
+                startDate: DateTime.now(),
+                endDate: DateTime.now(),
+                selectedLanguage: 'en',
+                selectedYear: '2022',
+              ),
+              child: ListView.builder(
+                controller: _scrollController,
+                itemCount: pr.books.length,
+                itemBuilder: (context, index) {
+                  final book = pr.books[index];
+                  return ListTile(
+                    title: Text(book.name),
+                    subtitle: Text('Year: ${book.year}'),
+                    // Другие поля, которые вы хотите отобразить для каждой книги
                   );
-                } else {
-                  return ListView.builder(
-                    itemCount: pr.books.length,
-                    itemBuilder: (context, index) {
-                      final book = pr.books[index];
-                      return ListTile(
-                        title: Text(book.name),
-                        subtitle: Text('Year: ${book.year}'),
-                        // Другие поля, которые вы хотите отобразить для каждой книги
-                      );
-                    },
-                  );
-                }
-              },
+                },
+              ),
             ),
           ),
         ],
@@ -62,3 +105,4 @@ class ScreenTwo extends StatelessWidget {
     );
   }
 }
+
